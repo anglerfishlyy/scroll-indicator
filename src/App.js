210ba62e8f1a0016cfecbf14f09e4b5b1fa0fb2a
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-06-03 15:11:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.jsx';
+import ScrollIndicator from './index.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> 
+    </div>
+  );
+}
+
+export default App;
